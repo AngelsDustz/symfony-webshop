04df7919ed1de5d02ebfc5fec632d1a7b24b3f9e
Berwout Kruit <tehber@hotmail.com>
2020-04-01 22:06:04 +0200

    Add hello world, configure modules.

---
+++ myapp/src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * HomeController Class.
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * Home Action.
+     *
+     * @Route("/", methods={"GET"}, name="home")
+     */
+    public function home(): Response
+    {
+        return new Response('Hello World');
+    }
+}
